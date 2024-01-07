4d683c1942bcb4a0a6851b7269f3f11af3c8d5e7
Giovanni Pinto <giovanni.pinto@appliedblockchain.com>
2024-01-07 23:24:51 +0100

    Parallel execution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use rayon::iter::{Filter, ParallelBridge, ParallelIterator};
 use std::{env, fs};
 
 use image_actions::find_empty_space::find_margins;
@@ -15,24 +16,40 @@ fn main() {
     let images = fs::read_dir(&args[1])
         .unwrap()
         .filter(|entry| {
-            let binding = entry
-                .as_ref()
-                .unwrap()
-                .path();
-                
-            let file_name = binding.to_str()
-                .unwrap();
+            let binding = entry.as_ref().unwrap().path();
+
+            let file_name = binding.to_str().unwrap();
 
             file_name.ends_with(".png") || file_name.ends_with(".jpg")
-        });
+        })
+        .into_iter();
 
-    for file in images {
+    images.par_bridge().for_each(|file| {
         println!("Processing {}", file.as_ref().unwrap().path().display());
         let mut img = load_image(&file.as_ref().unwrap().path().display().to_string());
         let margins = find_margins(&mut img, 100.0);
-        let cropped_img = image_actions::crop(&mut img, margins.0.0, margins.0.1, margins.1.0 - margins.0.0, margins.1.1 - margins.0.1);
+        let cropped_img = image_actions::crop(
+            &mut img,
+            margins.0 .0,
+            margins.0 .1,
+            margins.1 .0 - margins.0 .0,
+            margins.1 .1 - margins.0 .1,
+        );
         let new_image_path = "./cropped/".to_owned() + &file.unwrap().path().display().to_string();
         println!("Saving image to {}", new_image_path);
-        cropped_img.to_image().save(new_image_path).expect("Failed to save image");
-    }
+        cropped_img
+            .to_image()
+            .save(new_image_path)
+            .expect("Failed to save image");
+    });
+
+    // for file in images {
+    //     println!("Processing {}", file.as_ref().unwrap().path().display());
+    //     let mut img = load_image(&file.as_ref().unwrap().path().display().to_string());
+    //     let margins = find_margins(&mut img, 100.0);
+    //     let cropped_img = image_actions::crop(&mut img, margins.0.0, margins.0.1, margins.1.0 - margins.0.0, margins.1.1 - margins.0.1);
+    //     let new_image_path = "./cropped/".to_owned() + &file.unwrap().path().display().to_string();
+    //     println!("Saving image to {}", new_image_path);
+    //     cropped_img.to_image().save(new_image_path).expect("Failed to save image");
+    // }
 }
