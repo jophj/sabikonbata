6ee13d9ae9ad5ba25b1bc4a635c99fabe3befcdd
Giovanni Pinto <giovanni.pinto@appliedblockchain.com>
2024-01-07 22:42:29 +0100

    Correctly cropping with the threshold method

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
+use std::env;
+
+use image_actions::find_empty_space::find_margins;
+
 mod image_actions;
 mod filesystem;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
     // Load the image (replace "input_image.png" with the path to your image)
-    let mut img = filesystem::load_image("input_image.jpg");
+    let mut img = filesystem::load_image(&args[1]);
 
     // Define the position and size of the crop (example: top left corner, width 100, height 100)
-    let (x, y, width, height) = (0u32, 0u32, 300u32, 300u32);
+    let margins = find_margins(&mut img, 100.0);
     
+    println!("Margins: {:?}", margins);
     // Perform the crop operation
-    let cropped_img = image_actions::crop(&mut img, x, y, width, height);
-        // Save the cropped image (replace "cropped_image.png" with the desired output path)
-    cropped_img.to_image().save("cropped_image.png").expect("Failed to save image");
+    let cropped_img = image_actions::crop(&mut img, margins.0.0 + 1, margins.0.1 + 1, margins.1.0 - margins.0.0 - 1, margins.1.1 - margins.0.1 - 1);
+    // Save the cropped image (replace "cropped_image.png" with the desired output path)
+    let new_image_path = "./cropped/".to_owned() + &args[1];
+    println!("Saving image to {}", new_image_path);
+    cropped_img.to_image().save(new_image_path).expect("Failed to save image");
 }
